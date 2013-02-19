50c19bbbffe5e0ca398e98dc6694ec197d9c1202
klsforever@gmail.com <klsforever@gmail.com@d444de68-b51b-ab58-7dcc-0ddd264d3237>
2013-02-19 01:55:37 +0000

    注入smali代码后重新打包apk并启动app的主程序

---
+++ trunk/onekey-decompile-apk/_inject_build.bat
@@ -0,0 +1,3 @@
+call _inject_classes E:\NetBeansProjects\xxInject\build\classes E:\androidWorkspace\onekey-decompile-apk\xx\smali
+call _build_sign_install xx
+adb shell am start -n xx.xx/.MainActivity
